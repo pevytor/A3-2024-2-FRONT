4cc9cf08875d13daff07ff5bd34fcc5b398b63a7
Pedro Henrique <BlueF3ny>
2024-11-18 20:46:20 -0300

    atualizar Fundo

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import { Login } from "@/components/login/Login";
 
 export default function Page() {
     return (
-        <main className="min-h-screen flex items-center justify-center">
+        <main className="min-h-screen flex items-center justify-center bg-blue-200">
              <Login />  
         </main>
     );
