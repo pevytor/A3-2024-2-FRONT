49bf00e7b9c7365a4c0036dbc26055c1afefdb63
Pedro Vytor <111099756+pevytor@users.noreply.github.com>
2024-12-08 14:17:48 -0300

    fix: corrigindo alias do webpack

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
 const path = require('path');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
-        source: '/api/:path*', // Proxy para o Next.js
-        destination: 'https://api-a3-asgz.onrender.com/:path*', // Alvo
+        source: '/api/:path*',
+        destination: 'https://api-a3-asgz.onrender.com/:path*',
       },
     ];
   },
